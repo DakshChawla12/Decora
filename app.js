09390f5c7e7cb953e81af4c5364b294fac78e457
Avikam <biravikam@gmail.com>
2025-03-02 19:23:18 +0530

    Implemented controllers and routes for design and department_design

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ const role_permissionsRoutes = require("./routes/role_permissionsRoutes");
 const permissionRoutes = require("./routes/permissionRoutes");
 const country_stateRoutes = require("./routes/country_stateRoutes");
 const region_countryRoutes = require("./routes/region_countryRoutes");
+const designation_routes = require("./routes/designationRoutes");
+const department_designation_routes = require("./routes/department_designRoutes");
 
 // *** Setting the routers *** //
 app.use("/api/country", countryRoutes);
@@ -30,9 +32,11 @@ app.use("/api/region", regionRoutes);
 app.use("/api/role", roleRoutes);
 app.use("/api/employee", employeeRoutes);
 app.use("/api/role-permissions", role_permissionsRoutes);
-app.use("/api/permission",permissionRoutes);
+app.use("/api/permission", permissionRoutes);
 app.use("/api/country-state", country_stateRoutes);
 app.use("/api/region-country", region_countryRoutes);
+app.use("/api/designation", designation_routes);
+app.use("/api/department-design", department_designation_routes);
 
 // *** Connect to database first and sync tables ***
 connectDB()
