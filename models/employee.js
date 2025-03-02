4cc26f6457ab191884fd8989502b0479f30903e8
Ashish Karakoti <ashishkarakoti8@gmail.com>
2025-03-02 21:49:53 +0530

    Completed controllers for Employee

--- models/employee.js
+++ models/employee.js
@@ -1,53 +1,51 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const {sequelize} = require('../config/database');
+const { Sequelize, DataTypes } = require("sequelize");
+const { sequelize } = require("../config/database");
 
-const Employee = sequelize.define('Employee', {
-            Empid: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-        
-    },
-    FName: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    LName: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-     Deptid:{
-            type: DataTypes.INTEGER,
-            allowNull : false
-     },
-     Desigid:{
-            type: DataTypes.INTEGER,
-            allowNull : false
-     },
-   Address:{
-            type: DataTypes.STRING,
-            allowNull: false
-   },
-   doj:{
-            type: DataTypes.DATE,
-            allowNull: false
-   },
-   stateid:{
+const Employee = sequelize.define("Employee", {
+  Empid: {
     type: DataTypes.INTEGER,
-    allowNull: true
-},
-countryid:{
-    type: DataTypes.INTEGER,
-    allowNull: false
-},
-regionid:{
-    type: DataTypes.INTEGER,
-    allowNull: false
-},
-userid:{
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  FName: {
     type: DataTypes.STRING,
-    allowNull: false
-},
-
+    allowNull: false,
+  },
+  LName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  Deptid: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  Desigid: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  Address: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  doj: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  stateid: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  countryid: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  regionid: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  userid: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
 });
 module.exports = Employee;
